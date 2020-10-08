ce3c7d4f49074322f4759062ec1a53c3b7df7bf0
Keszeri <56658849+Keszeri@users.noreply.github.com>
2020-10-08 11:01:48 +0200

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <title>Nazwa.PL :: Serwer minecraft</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+</head>
+<body>
+    <!-- Elment blokowy główny. -->
+    <div class="head">
+        <p>Head</p>
+    </div>
+    <!-- Stopka strony głównej -->
+    <footer>
+        <p>GoldForest.PL - All right reserved.</p>
+    </footer>
+</body>
+</html>
